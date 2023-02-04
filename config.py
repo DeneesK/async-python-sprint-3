8d41b68a3605d092c05526c0779503c5ef678cf8
D Kalb <kds-ru@list.ru>
2023-02-04 16:40:36 +0100

    readme

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
     buffer_size: int = Field(20)
     client_info_filepath: str = Field('client.json')
-    limit_update_period: int = Field(60 * 60)  # seconds
-    file_size_limit: int = Field(5000 * 1000)  # bytes
     client_host: str = Field('http://127.0.0.1')
     server_host: str = Field('127.0.0.1')
     port: int = Field(8000)
